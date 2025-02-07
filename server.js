9d33ed0b7747f3cbb3c8450744f766005602515c
rahulrr-coder <rahulrr5002@protonmail.com>
2025-02-07 09:37:06 +0530

    MongoDB Connection

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express()
 const PORT = process.env.PORT || 3000;
+const connectDatabase = require('./DataBase');
+
+connectDatabase();
 
 app.get('/',function(req,res){
     res.send('Backend server is live!')
