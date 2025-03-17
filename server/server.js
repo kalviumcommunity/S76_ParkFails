3c902797d8d6901b2cbb48f0e0976c64314bf3bb
rahulrr-coder <rahulrr5002@protonmail.com>
2025-03-17 15:13:42 +0530

    Resolve backend api errors

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express()
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 const Database = require('./DataBase');
 app.use(express.json());  
